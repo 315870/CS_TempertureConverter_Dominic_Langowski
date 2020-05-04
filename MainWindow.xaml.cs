8dd4d2ab80d1635d330625b4c60c0a0dbae558d7
315870 <60711142+315870@users.noreply.github.com>
2020-05-04 12:24:35 -0400

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TempertureConventer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnConvert_Click(object sender, RoutedEventArgs e)
+        {
+            // Values
+            double inputCelsius;
+            double inputFahreneit;
+            double ConvertCtoF;
+            double ConvertFtoC;
+            // Values
+
+            //User Input 
+            double.TryParse(txtCelsuisInput.Text, out inputCelsius);
+
+            double.TryParse(txtFahrenheitInput.Text, out inputFahreneit);
+            //User Input
+
+            // Conversation
+            ConvertCtoF = ((inputCelsius * 9)/5) + 32;
+            //MessageBox.Show(ConvertCtoF.ToString());
+
+            ConvertFtoC = ((inputFahreneit - 32) * 5)/9;
+            //MessageBox.Show(ConvertFtoC.ToString());
+            // Conversation
+
+            // Output titles visibility
+            lblCelsuisTitleOutput.Visibility = Visibility.Visible;
+
+            lblFahrenheitInputTitle.Visibility = Visibility.Visible;
+            // Output titles visibility
+
+            //Output tempertures
+            lblCelsuisOutput.Content = ConvertCtoF.ToString("#.0") + " F";
+
+            lblFahrenheitOutput.Content = ConvertFtoC.ToString("#.0") + " C";
+            //Output tempertures
+
+
+
+
+
+
+
+
+        }
+    }
+}
